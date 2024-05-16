48e5ec16bcdd895773f71d3976f469d0606a71b0
dzhamaldinov-maksim <116875290+dzhamaldinov-maksim@users.noreply.github.com>
2024-05-16 22:31:49 +0200

    corrections

--- addition.py
+++ addition.py
@@ -1,6 +1,6 @@
 """dzhamaldinov_part_of_code."""
 
 
-def addition(num1, num2):
-    """addition"""
+def addition(num1: int, num2: int) -> int:
+    """addition."""
     return num1 + num2  # Возвращаем результат сложения
