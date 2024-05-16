f4f7872fbb825b6a46a70894e9df5f3eccf21cb4
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-05-16 22:35:54 +0300

    Update run.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- run.py
+++ run.py
@@ -3,11 +3,6 @@
 import time
 
 
-def multiplication(num_1: int, num_2: int) -> int:
-    """Multiplication."""
-    return num_1 * num_2  # Возвращаем результат умножения
-
-
 def calculator_input() -> None:
     "Input."
     otwet = 1
