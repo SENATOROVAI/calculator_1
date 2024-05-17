394c01ecfbfa834f631eb6d16f5ea78d9d998fc8
dzhamaldinov-maksim <116875290+dzhamaldinov-maksim@users.noreply.github.com>
2024-05-17 21:09:14 +0200

    fixed description

--- addition.py
+++ addition.py
@@ -2,5 +2,17 @@
 
 
 def addition(num1: int, num2: int) -> int:
-    """The addition function. We get result of addit some numbers."""
-    return num1 + num2  # Возвращаем результат сложения
+    """
+    The function `addition` takes two integer inputs and returns their sum.
+
+    Args:
+      num1 (int): The parameter `num1` is an integer that represents the first
+      number to be added in the
+    addition function.
+      num2 (int): num2 is the second integer number that will be added to num1
+      in the addition function.
+
+    Returns:
+      The function `addition` is returning the sum of `num1` and `num2`.
+    """
+    return num1 + num2
